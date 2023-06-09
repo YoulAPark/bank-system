726d42b9c2503735eacfd90d9f7581c4d8c4ba1e
ouneno <dbfdk2531@gmail.com>
2023-06-09 23:48:56 +0900

    2023-06-09 23:48 Commit

--- bank-system/src/main/java/com/ouneno/banksystem/controller/HomeController.java
+++ bank-system/src/main/java/com/ouneno/banksystem/controller/HomeController.java
@@ -3,20 +3,21 @@ package com.ouneno.banksystem.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.ouneno.banksystem.service.user.serviceImpl.userService;
 
 @Controller
-@RestController
+@RequestMapping
 public class HomeController {
 
 	@Autowired
 	private userService userService;
-	
+
 	@RequestMapping("/")
 	public String main() throws Exception {
-		return "main";
+		String a = "string";
+		System.out.println(a);
+		return "/main";
 	}
 
 }
