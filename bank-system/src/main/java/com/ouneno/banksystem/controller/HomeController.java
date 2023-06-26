e8a7c60780daeed82882ef03decaf4526f0a4930
ouneno <dbfdk2531@gmail.com>
2023-06-26 22:28:47 +0900

    2023-06-26 22:28 Annotation 오설정으로 인한 서버작동 오류. 해결완

--- bank-system/src/main/java/com/ouneno/banksystem/controller/HomeController.java
+++ bank-system/src/main/java/com/ouneno/banksystem/controller/HomeController.java
@@ -3,21 +3,20 @@ package com.ouneno.banksystem.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.ouneno.banksystem.service.user.serviceImpl.userService;
 
 @Controller
-@RequestMapping
+@RestController
 public class HomeController {
 
 	@Autowired
 	private userService userService;
-
+	
 	@RequestMapping("/")
 	public String main() throws Exception {
-		String a = "string";
-		System.out.println(a);
-		return "/main";
+		return "main";
 	}
 
 }
