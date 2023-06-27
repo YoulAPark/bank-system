3704ce8a7ed5495785e42c6b6dfbc34f681f7c00
ouneno <dbfdk2531@gmail.com>
2023-06-27 22:27:00 +0900

    2023-06-27b 22:26 build.gradle Controller에서 jsp인식 오류로 springframework 버전 변경

--- bank-system/src/main/java/com/ouneno/banksystem/controller/HomeController.java
+++ bank-system/src/main/java/com/ouneno/banksystem/controller/HomeController.java
@@ -3,12 +3,11 @@ package com.ouneno.banksystem.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.ouneno.banksystem.service.user.serviceImpl.userService;
 
 @Controller
-@RestController 
+@RequestMapping
 public class HomeController {
 
 	@Autowired
